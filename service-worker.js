1c51f256b4d592ae6f327f434d26cbeaf291bae6
mek-kb <mutrikukoeskolakirola@gmail.com>
2026-07-16 10:43:54 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "eskola-kirola-v4";
+const CACHE_NAME = "eskola-kirola-v5";
 
 const FILES_TO_CACHE = [
   "./",
@@ -6,22 +6,47 @@ const FILES_TO_CACHE = [
   "./style.css",
   "./app.js",
   "./manifest.json"
-  "./images/lh3-ordutegia.jpg",
-  "./images/lh4-ordutegia.jpg",
-  "./images/lh5-ordutegia.jpg",
-  "./images/dbh-ordutegia.jpg"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const responseCopy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseCopy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
